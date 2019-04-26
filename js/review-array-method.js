14b6022626935401558a40732d84bd9ae7226567
Emily Vagim <emily@Emilys-MacBook-Pro-3.local>
2019-04-25 18:53:18 -0700

    array review

---
+++ js/review-array-method.js
@@ -0,0 +1,21 @@
+var arr1 = ['papya', 'banana', 'pluot'];
+console.log(arr1);
+
+console.log(arr1[0]);
+console.log(arr1[1]);
+console.log(arr1[2]);
+
+arr1.unshift('kiwi');
+console.log(arr1);
+
+arr1.push('honey dew');
+console.log(arr1);
+
+arr1.splice(2, 1, 'apple');
+console.log(arr1);
+
+arr1.splice(1, 1);
+console.log(arr1);
+
+var arr2 = ['papya', 'banana', 'pluot'];
+console.log(arr2);
