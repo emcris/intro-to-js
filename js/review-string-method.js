db1c367b3ba2af1c253e360717594d46baaf2acd
Emily <49257572+emcris@users.noreply.github.com>
2019-04-25 18:42:58 -0700

    Review string methods (#4)
    * hello world
    
    * review string methods

---
+++ js/review-string-method.js
@@ -0,0 +1,14 @@
+var str1 = 'Hello World!';
+
+console.log(str1);
+console.log(str1.slice(0,6));
+console.log(str1.slice(6,11));
+
+var make = 'Subaru';
+var modle = 'Outback'
+
+var modle = make + ' ' + modle;
+console.log(modle);
+
+var make = modle.concat(' ');
+console.log(make);
